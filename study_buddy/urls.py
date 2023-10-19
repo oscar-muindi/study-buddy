5598c3104e8de49e84ae0e09f98e357096157694
oscar-muindi <oscarmuindi@gmail.com>
2023-10-19 17:17:11 +0300

    added staticfiles for admin

--- study_buddy/urls.py
+++ study_buddy/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
@@ -13,3 +14,5 @@ urlpatterns = [
 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
